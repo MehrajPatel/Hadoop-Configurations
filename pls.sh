5507917f34f981c6e82ee023f9061f5186fecc3a
Mehraj Patel <mehrajpatel13@gmail.com>
2020-12-27 10:49:36 +0530

    Update pls.sh

--- pls.sh
+++ pls.sh
@@ -27,9 +27,9 @@ do
  ssh $username@$i 'chmod a+x pre-req.sh'
 done
 
-sudo rm ~/authorized_keys
-sudo rm $HOME/key.pem
-sudo rm $HOME/pre-req.sh
+#sudo rm ~/authorized_keys
+#sudo rm $HOME/key.pem
+#sudo rm $HOME/pre-req.sh
 
 sudo wget  -O $HOME/jdk-8u131-linux-x64.rpm --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
 
